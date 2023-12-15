833e651bddb3b2ffd1897bfbce16bc8c0ecf1f30
Shweta Mohan Bharambe <shwetabharambe21@gmail.com>
2023-12-15 17:52:25 +0530

    nineth commit

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const http = require('http');
 
-const server = http.createServer((req, res)=>{
-    console.log(req.url, req.method, req.headers);
+const routes = require('./router');
 
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Shweta Mohan Bharambe</h2></body>');
-    res.write('</html>');
-    res.end();
-});
+console.log(routes.someText);
 
+//const server = http.createServer(routes);
 
-server.listen(4000);
+const server = http.createServer(routes.handler);
+
+server.listen(5000);
